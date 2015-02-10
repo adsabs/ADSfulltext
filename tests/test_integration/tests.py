f6f8586c8a3cfd2bd36c6b83100dbf2f1a5cba4a
Jonny Elliott <jonnynelliott@googlemail.com>
2015-02-10 17:29:47 -0500

    FTs have been tidied up and now tearDown takes care of queues with linger payloads. FTs are passing correctly now. Extra ones needed to further test.

--- tests/test_integration/tests.py
+++ tests/test_integration/tests.py
@@ -34,27 +34,36 @@ class TestExtractWorker(unittest.TestCase):
             os.remove('/vagrant/tests/test_unit/stub_data/te/st/1/fulltext.txt')
             os.rmdir('/vagrant/tests/test_unit/stub_data/te/st/1/')
 
+        # Purge the queues if they have content
+        channel_list = [[self.check_worker.channel, 'CheckIfExtractQueue'],
+                        [self.standard_worker.channel, 'StandardFileExtractorQueue'],
+                        [self.meta_writer.channel, 'WriteMetaFileQueue'],
+                        ]
+
+        for channel_link, queue_name in channel_list:
+            single_connection = channel_link.queue_purge(queue=queue_name)
 
     def test_extraction_of_non_extracted(self):
 
-        # user loads the list of full text files
-        test_publish = 'tests/test_integration/stub_data/fulltext.links'
+        # user loads the list of full text files and publishes them to the first queue
+        test_publish = 'tests/test_integration/stub_data/fulltext_single_document.links'
         records = read_links_from_file(test_publish)
 
-        with open(PROJ_HOME + "/" + test_publish, "r") as f:
+        with open(os.path.join(PROJ_HOME, test_publish), "r") as f:
             lines = f.readlines()
             nor = len(lines)
 
-        number_of_PDFs = len(list(filter(lambda x: x.lower().endswith('.pdf'), [i.strip().split("\t")[-2] for i in lines])))
+        number_of_PDFs = len(list(filter(lambda x: x.lower().endswith('.pdf'),
+                                         [i.strip().split("\t")[-2] for i in lines])))
         number_of_standard_files = nor - number_of_PDFs
 
         self.assertEqual(len(records.bibcode), nor)
 
-        # Converts the input into a user defined payload
+        # The pipeline converts the input into a payload expected by the workers
         records.make_payload()
         self.assertTrue(len(records.payload)>0)
 
-        # Queues and routes are started
+        # Queues and routes are switched on so that they can allow workers to connect
         TM = TaskMaster(psettings.RABBITMQ_URL, psettings.RABBITMQ_ROUTES, psettings.WORKERS)
         TM.initialize_rabbitmq()
 
@@ -63,23 +72,25 @@ class TestExtractWorker(unittest.TestCase):
         ret_queue = publish_worker.connect(psettings.RABBITMQ_URL)
         self.assertTrue(ret_queue)
 
-        # External worker publishes to the rabbitmq queue
+        # External worker publishes the payload created before to the RabbitMQ queue
+        # for the workers to start consuming
         ret = publish(publish_worker, records.payload, exchange='FulltextExtractionExchange',
                       routing_key='CheckIfExtractRoute')
         self.assertTrue(ret)
         time.sleep(10)
 
         # Worker receives packet of information and checks to see if it needs to be updated
-        # time.sleep(5) do not use time.sleep,
-        # see: http://stackoverflow.com/questions/22061082/\
-        # getting-pika-exceptions-connectionclosed-error-while-using-rabbitmq-in-python
-
+        ## see: http://stackoverflow.com/questions/22061082/\
+        ## getting-pika-exceptions-connectionclosed-error-while-using-rabbitmq-in-python
         print('starting check worker...')
         self.check_worker.run()
+
+        # We pause to simulate the asynchronous running of the workers. This is not needed when the workers
+        # are listening continuously.
         time.sleep(10)
 
         # Check to see if the correct number of updates got published to the next queue
-        # Re-declare the queue with passive flag
+        ## Re-declare the queue with passive flag
         standard_queue = self.check_worker.channel.queue_declare(
             queue="StandardFileExtractorQueue",
             passive=True
@@ -90,7 +101,14 @@ class TestExtractWorker(unittest.TestCase):
             passive=True
             )
 
-        print self.check_worker.results
+        self.assertTrue(standard_queue.method.message_count == number_of_standard_files,
+                        "Standard queue should have at least %d message, but it has: %d" %
+                        (number_of_standard_files, standard_queue.method.message_count))
+        self.assertTrue(pdf_queue.method.message_count == number_of_PDFs,
+                        "PDF queue should have at least %d message, but it has: %d" %
+                        (number_of_PDFs, pdf_queue.method.message_count))
+
+        # Double check with the worker output
         pdf_res = json.loads(self.check_worker.results["PDF"])
         standard_res = json.loads(self.check_worker.results["Standard"])
 
@@ -98,15 +116,10 @@ class TestExtractWorker(unittest.TestCase):
             pdf_res = len(pdf_res)
         else:
             pdf_res = 0
-        self.assertEqual(pdf_res, number_of_PDFs, 'Expected number of PDFs: %d' % number_of_PDFs)
-        self.assertEqual(len(standard_res), number_of_standard_files, 'Expected number of normal formats: %d' % number_of_standard_files)
-        # self.assertEqual(self.check_worker.results, 'pass')
-        self.assertTrue(pdf_queue.method.message_count == pdf_res,
-                        "PDF queue should have at least %d message, but it has: %d" % (pdf_res, pdf_queue.method.message_count))
 
-        # Clean-up for next test: should be removed when next queue implemented
-        # self.check_worker.channel.queue_purge(queue="StandardFileExtractorQueue")
-        self.check_worker.channel.queue_purge(queue="PDFFileExtractorQueue")
+        self.assertEqual(pdf_res, number_of_PDFs, 'Expected number of PDFs: %d' % number_of_PDFs)
+        self.assertEqual(len(standard_res), number_of_standard_files, 'Expected number of normal formats: %d' %
+                         number_of_standard_files)
 
         # There should be no errors at this stage
         queue_error = self.check_worker.channel.queue_declare(
@@ -116,47 +129,22 @@ class TestExtractWorker(unittest.TestCase):
         self.assertTrue(queue_error.method.message_count == 0,
                         "Should be 0, but it is: %d" % queue_error.method.message_count)
 
-        # Standard Extractor should extract the content of the given payload
+        # Now the next worker collects the list of files that need to be extracted. The Standard
+        # Extractor should extract the content of the given payload and so the number of outputs
+        # should match the number before. Given we don't expect any errors here!
         print('starting extractor worker')
         self.standard_worker.run()
-        # standard_res = json.loads(self.standard_worker.results)[0]
-        # self.assertItemsEqual(META_CONTENT["XML"].keys(), standard_res.keys())
-        #
-        # standard_res = json.loads(self.standard_worker.results)[1]
-        # self.assertTrue(u'fulltext' in standard_res.keys())
-        #
-        # standard_res = json.loads(self.standard_worker.results)[2]
-        # self.assertTrue(u'fulltext' in standard_res.keys())
-        #
-        # standard_res = json.loads(self.standard_worker.results)[2]
-        # self.assertTrue(u'fulltext' in standard_res.keys())
-        #
-        # standard_res = json.loads(self.standard_worker.results)[3]
-        # self.assertTrue(u'fulltext' in standard_res.keys())
-        # self.assertTrue(u'acknowledgements' in standard_res.keys())
+        number_of_standard_files_2 = len(json.loads(self.standard_worker.results))
+        self.assertTrue(number_of_standard_files_2, number_of_standard_files)
 
-        # self.assertEquals(len(json.loads(self.standard_worker.results)), 4)
+        # After the extractor, the meta writer should write all the payloads to disk in the correct
+        # folders
         print('starting meta writer...')
         self.meta_writer.run()
 
-
         time.sleep(5)
-        # The writing queue should now contain the correct number in the payload
-        queue_write = self.check_worker.channel.queue_declare(
-            queue="WriteMetaFileQueue",
-            passive=True
-            )
-
         self.assertTrue(os.path.exists('/vagrant/tests/test_unit/stub_data/te/st/1/meta.json'))
-
-        # # When extracted, the payload should no longer exist within the standard file queue
-        # self.assertTrue(standard_queue.method.message_count == 0,
-        #                 "Standard queue should have 1 message, but it has: %d" % standard_queue.method.message_count)
-
-        # The content is extracted and passed on as a payload, it now must be written to file
-
-        # ErrorHandler should go through any of the problems that arose and deal with them
-
+        self.assertTrue(os.path.exists('/vagrant/tests/test_unit/stub_data/te/st/1/fulltext.txt'))
 
 if __name__ == "__main__":
     unittest.main()
