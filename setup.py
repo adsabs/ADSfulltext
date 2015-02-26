4d14086303811c7193fdd5a5cfc72c3d89c3c1be
Jonny Elliott <jonnynelliott@googlemail.com>
2015-02-26 15:53:30 -0500

    Setup for easy use on travisCI, I personally prefer editing PYTHONPATH, but see: http://stackoverflow.com/questions/25756134/how-to-add-my-module-to-travis-ci-pythonpath

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='ADSfulltext',
+  version='0.1',
+  description='ADS full text extraction pipeline',
+  author='Jonny Elliott',
+  author_email='jonnynelliott@googlemail.com',
+  url='www.jonnyelliott.org',
+  packages=['.', 'lib', 'pipeline'],
+)
