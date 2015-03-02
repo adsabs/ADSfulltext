df14f50958ef9dcf6712e1aaedf6eb6164b59ca5
Jonny Elliott <jonnynelliott@googlemail.com>
2015-03-02 17:41:37 -0500

    First commit of the PDFExtractor Java package

---
+++ src/main/java/org/adslabs/adsfulltext/App.java
@@ -0,0 +1,13 @@
+package org.adslabs.adsfulltext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
