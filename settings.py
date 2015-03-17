b5f946adb5336e16651d5ce8cf9bdb6ce46ee051
Jonny Elliott <jonnynelliott@googlemail.com>
2015-03-17 16:11:03 -0400

    Functional tests updated such that they test both the Java and Python codebases. All the tests are currently passing. The functional tests should be done by hand and not by travis though

--- settings.py
+++ settings.py
@@ -52,7 +52,8 @@ META_CONTENT = {
     },
     "txt": {"fulltext": [""]},
     "ocr": {"fulltext": [""]},
-    "http": {"fulltext": [""]}
+    "http": {"fulltext": [""]},
+    "pdf": {"fulltext": [""]},
 }
 
 
--- tests/test_functional/test_extraction.py
+++ tests/test_functional/test_extraction.py
@@ -13,7 +13,16 @@ class TestExtractWorker(TestGeneric):
     def setUp(self):
         self.expected_folders = []
         # Start the pipeline
-        self.supervisor_ADS_full_text('start')
+        try:
+            self.supervisor_ADS_full_text(action='start', pipeline='Python')
+        except:
+            print "WARNING: COULD NOT START PYTHON PIPELINE"
+            pass
+        try:
+            self.supervisor_ADS_full_text(action='start', pipeline='Java')
+        except:
+            print "WARNING: COULD NOT START JAVA PIPELINE"
+            pass
         time.sleep(3)
 
     def tearDown(self):
@@ -22,25 +31,34 @@ class TestExtractWorker(TestGeneric):
         self.clean_up_path(self.expected_folders)
 
         # Stop the pipeline
-        self.supervisor_ADS_full_text('stop')
+        try:
+            self.supervisor_ADS_full_text(action='stop', pipeline='Java')
+        except:
+            print "WARNING: COULD NOT STOP JAVA PIPELINE"
+            pass
+        try:
+            self.supervisor_ADS_full_text(action='stop', pipeline='Python')
+        except:
+            print "WARNING: COULD NOT STOP PYTHON PIPELINE"
+            pass
 
+    def supervisor_ADS_full_text(self, action, pipeline):
 
-
-    def supervisor_ADS_full_text(self, action):
+        pipeline_d = {'Python': 'ADSfulltext',
+                      'Java': 'ADSfulltextPDFLIVE'}
 
         accepted_actions = ['stop', 'start']
         if action not in accepted_actions:
             print 'You can only use: %s' % accepted_actions
-            sys.exit(0)
+            return
 
-        process = subprocess.Popen(['supervisorctl', '-c', '/vagrant/supervisord.conf', action, 'ADSfulltext'],
+        process = subprocess.Popen(['supervisorctl', '-c', '/etc/supervisord.conf', action, pipeline_d[pipeline]],
                                    stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
         output, error = process.communicate()
 
         if action not in output:
-            print 'ADSfull text could not be started correctly, exiting'
-            sys.exit(0)
+            raise Exception('ADSfull text could not be started correctly, exiting', output, error)
 
     def test_functionality_of_the_system_on_non_existent_files(self):
 
@@ -48,15 +66,17 @@ class TestExtractWorker(TestGeneric):
         # Obtain the parameters to publish to the queue
         # Expect that the records are split into the correct number of
         # packet sizes
-        run.run(full_text_links=full_text_links,
+        run.run(full_text_links=os.path.join(PROJ_HOME, full_text_links),
                  packet_size=10)
 
-        time.sleep(60)
+        time.sleep(10)
 
         self.expected_folders = self.calculate_expected_folders(full_text_links)
 
         for expected_f in self.expected_folders:
             self.assertTrue(os.path.exists(expected_f), 'Could not find: %s' % expected_f)
+            self.assertTrue(os.path.exists(os.path.join(expected_f, 'meta.json')), 'Could not find: %s' % os.path.join(expected_f, 'meta.json'))
+            self.assertTrue(os.path.exists(os.path.join(expected_f, 'fulltext.txt')), 'Could not find: %s' % os.path.join(expected_f, 'fulltext.txt'))
 
 
 if __name__ == "__main__":
