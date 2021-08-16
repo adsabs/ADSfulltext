fbf95defac7d4d68f3ae5c4a317336d8b063c8f0
Nemanja Martinovic <nemanja.martinovic@cfa.harvard.edu>
2021-08-16 08:37:01 -0400

    Expand script around pdftotext to handle issues with hanging

--- scripts/extract_pdf_with_pdftotext.sh
+++ scripts/extract_pdf_with_pdftotext.sh
@@ -3,7 +3,35 @@
 # Call the PDF extractor and returns (writes into STDOUT) its output
 
 if [ ! -e $1 ]; then
-	exit 127 # not found
+        exit 127 # not found
 fi
 
-/usr/bin/pdftotext -enc UTF-8 -eol unix -q "$1"  -
+EXITCODE=0
+STD_OUT="$(mktemp)"
+
+#/usr/bin/pdftotext -enc UTF-8 -eol unix -q "$1"  -
+
+timeout -s SIGINT 30s /usr/bin/pdftotext -enc UTF-8 -eol unix -q "$1"  - > $STD_OUT
+
+if [ $? -eq 124 ]
+then
+   VECTOR_FREE="$(mktemp)"
+   gs -q -o $VECTOR_FREE -sDEVICE=pdfwrite -dFILTERVECTOR "$1"
+   timeout -s SIGINT 30s /usr/bin/pdftotext -enc UTF-8 -eol unix -q "$VECTOR_FREE" - > $STD_OUT
+   EXITCODE=$?
+
+   rm $VECTOR_FREE
+
+   if [ $EXITCODE -eq 124 ]
+   then
+        echo "PDFTOTEXT ERROR! Can't process file $1 within 30 seconds!"
+        rm $STD_OUT
+        exit $EXITCODE
+   fi
+
+fi
+
+cat $STD_OUT
+rm $STD_OUT
+
+exit $EXITCODE
