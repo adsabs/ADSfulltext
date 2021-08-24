951de7841e1777e82cede7b7faba83722cd15e02
Nemanja Martinovic <nemanja.martinovic@cfa.harvard.edu>
2021-08-24 15:47:22 -0400

    Extend script with comments and introduce timeout system variable

--- scripts/extract_pdf_with_pdftotext.sh
+++ scripts/extract_pdf_with_pdftotext.sh
@@ -6,18 +6,24 @@ if [ ! -e $1 ]; then
         exit 127 # not found
 fi
 
+# if PDFTOTEXT_TIMEOUT is null or unset, use 30s as timeout. The 30s was selected not 
+# to exclude articles that take longer processing (~10 or so seconds) but to limit 
+# the vector graphics stripping just to ones that are hanging
+PDFTOTEXT_TIMEOUT=${PDFTOTEXT_TIMEOUT:-30s}
 EXITCODE=0
 STD_OUT="$(mktemp)"
 
 #/usr/bin/pdftotext -enc UTF-8 -eol unix -q "$1"  -
 
-timeout -s SIGINT 30s /usr/bin/pdftotext -enc UTF-8 -eol unix -q "$1"  - > $STD_OUT
+# if it doesn't finish processing in 30 seconds assume that vector graphics
+# is blocking processing and proceed to strip it (temporarily) with ghostscript, then reprocess
+timeout -s SIGINT $PDFTOTEXT_TIMEOUT /usr/bin/pdftotext -enc UTF-8 -eol unix -q "$1"  - > $STD_OUT
 
 if [ $? -eq 124 ]
 then
    VECTOR_FREE="$(mktemp)"
    gs -q -o $VECTOR_FREE -sDEVICE=pdfwrite -dFILTERVECTOR "$1"
-   timeout -s SIGINT 30s /usr/bin/pdftotext -enc UTF-8 -eol unix -q "$VECTOR_FREE" - > $STD_OUT
+   timeout -s SIGINT $PDFTOTEXT_TIMEOUT /usr/bin/pdftotext -enc UTF-8 -eol unix -q "$VECTOR_FREE" - > $STD_OUT
    EXITCODE=$?
 
    rm $VECTOR_FREE
